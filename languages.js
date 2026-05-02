a6886cf604882745d894496d9aadb3fbe6b4b49c
Nawab512 <ldfjdnfndndnfjf@gmail.com>
2026-05-02 19:54:37 +0500

    Create languages.js

---
+++ languages.js
@@ -0,0 +1,12 @@
+const countries = {
+    "en-GB": "English",
+    "ur-PK": "Urdu",
+    "ar-SA": "Arabic",
+    "hi-IN": "Hindi",
+    "fr-FR": "French",
+    "es-ES": "Spanish",
+    "tr-TR": "Turkish",
+    "zh-CN": "Chinese",
+    "ru-RU": "Russian"
+    // Aap yahan mazeed codes add kar sakty hain
+};
